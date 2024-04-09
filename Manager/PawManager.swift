b78bdd499081112380c783c1590791f659e7166c
“twoown” <admin@twown.com>
2024-04-09 23:53:10 +0800

    change version

--- Manager/PawManager.swift
+++ Manager/PawManager.swift
@@ -28,7 +28,7 @@ class pawManager: ObservableObject{
     @Published var notificationPermissionStatus: UNAuthorizationStatus = .notDetermined
 
     
-    
+    private let session = URLSession(configuration: .default)
     
     
     private var cancellables: Set<AnyCancellable> = []
@@ -137,7 +137,7 @@ extension pawManager{
     func health(url: String) async-> Bool {
        
         do{
-            if let health: String = try await NetWorkManager.shared.fetchRaw(url: url){
+            if let health: String = try await fetchRaw(url: url){
                 return health == "ok"
             }
         }catch{
@@ -216,7 +216,7 @@ extension pawManager{
         }
         
         do {
-            if let deviceInfo:DeviceInfo? = try await NetWorkManager.shared.fetch(url: server.url + "/register/" + self.deviceToken + "/" + servers[index].key){
+            if let deviceInfo:DeviceInfo? = try await fetch(url: server.url + "/register/" + self.deviceToken + "/" + servers[index].key){
                 
                 self.dispatch_sync_safely_main_queue {
                     servers[index].key = deviceInfo?.pawKey ?? ""
@@ -326,5 +326,30 @@ extension pawManager{
 extension pawManager{
     func addQuickActions(){
         UIApplication.shared.shortcutItems = QuickAction.allShortcutItems
+    }
+    
+    func fetch<T:Codable>(url:String) async throws -> T?{
+        guard let requestUrl = URL(string: url) else {return  nil}
+        let data = try await session.data(for: URLRequest(url: requestUrl))
+        let result = try JSONDecoder().decode(baseResponse<T>.self, from: data)
+        return result.data
+    }
+    
+    func fetchRaw<T:Codable>(url:String) async throws -> T?{
+        guard let requestUrl = URL(string: url) else {return  nil}
+        let data = try await session.data(for: URLRequest(url: requestUrl))
+        let result = try JSONDecoder().decode(T.self, from: data)
+        return result
+    }
+    
+    func fetchVoid(url:String) async-> Bool {
+        guard let requestUrl = URL(string: url) else { return false }
+        do{
+            _ = try await session.data(for: URLRequest(url: requestUrl))
+            return true
+        }catch{
+           return false
+        }
+        
     }
 }
